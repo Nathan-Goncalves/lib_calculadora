411e190237501b330f6f9d6be42f019a5bd3407b
Nathan Goncalves <nathanwow12@gmail.com>
2025-06-19 14:14:50 -0300

    cria tutorial no readmme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # lib_calculadora
+## Como usar
+
+```python
+from lib_calculadora import dividir
+
+resultado = dividir(10, 2)
+print(resultado)  # 5.0
+```
--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="minha_calculadora",
-    version="0.1.0",
+    name="lib_calculadora",
+    version="1.0",
     packages=find_packages(),
     description="Uma biblioteca de operações matemáticas com FastAPI exceptions.",
-    author="Seu Nome",
-    author_email="seu@email.com",
+    author="Nathan Goncalves",
+    author_email="nathanwow12@gmail.com",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
     install_requires=[
         "fastapi"
     ]
